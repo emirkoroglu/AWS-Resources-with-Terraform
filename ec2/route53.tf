cc0ea325a6ed51916c580c5dae243a7b90ffe594
emirsway <emirmails@gmail.com>
2021-11-04 18:32:29 +0000

    output for ec2

--- ec2/route53.tf
+++ ec2/route53.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "blog" {
+resource "aws_route53_record" "wordpress" {
   zone_id = "Z0875892209G6JMLAI3PB"
-  name    = "blog.elonbitmusk.com"
+  name    = "wordpress.elonbitmusk.com"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.web.public_ip] 
